4cc78399674e80a577204ece550701a545f99551
DavidWendtNJIT <djw5383@gmail.com>
2022-06-06 18:59:14 -0400

    using context on the Gallery data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
-
+import { DataContext } from "./context/DataContext";
 
 const App = () => {
   let [search, setSearch] = useState("");
@@ -35,10 +35,11 @@ const App = () => {
     <div>
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 };
 
 export default App;
-
